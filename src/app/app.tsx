07d64bf36a06f068fd2e14cd9dadc3c3a85f0bad
LAGHZALI taha <mrtahalz@gmail.com>
2023-02-16 19:33:07 +0100

    initiliaze structure template

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="react-toolbox" />
+    </StyledApp>
+  );
+}
+
+export default App;
